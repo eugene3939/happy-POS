e3c11b97f8e1b011a1f191b884771e48e4fe6c4e
eugene <eugene2725@gmail.com>
2024-01-03 15:53:44 +0800

    add operation listVew int HomeFragment

--- MyPOS/app/src/main/java/com/example/mypos/ui/home/HomeFragment.kt
+++ MyPOS/app/src/main/java/com/example/mypos/ui/home/HomeFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.mypos.R
 import com.example.mypos.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -32,7 +35,17 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        // 修改這裡，使用 binding 對象找到 ListView
+        val listView: ListView = binding.listOperationSet
+        // 從字符串數組獲取操作列表
+        val operationArray = resources.getStringArray(R.array.operation_set)
+        // 使用 ArrayAdapter 來設置 ListView 的數據
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, operationArray)
+        // 設置 ListView 的 adapter
+        listView.adapter = adapter
         return root
+
     }
 
     override fun onDestroyView() {
